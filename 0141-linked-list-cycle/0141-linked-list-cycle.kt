a71efb8166ae4e5455453c1b327d512ff41c1e6c
Sendhy Ramadhinata <sendhyrama@gmail.com>
2023-08-25 14:23:37 +0700

    Time: 199 ms (39.91%), Space: 37.8 MB (38.65%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.kt
@@ -0,0 +1,25 @@
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class Solution {
+    fun hasCycle(head: ListNode?): Boolean {
+        var slow = head
+        var fast = head?.next
+        
+        while (fast != null && fast.next != null) {
+            if (slow == fast) {
+                return true
+            }
+            slow = slow?.next
+            fast = fast.next?.next
+        }
+        return false
+    }
+}

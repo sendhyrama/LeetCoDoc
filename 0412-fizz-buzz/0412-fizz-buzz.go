216963051e077faee1325642a70fdf1c0e65f43c
Sendhy Ramadhinata <sendhyrama@gmail.com>
2024-02-01 09:33:40 +0700

    Time: 5 ms (70.32%), Space: 3.5 MB (74.97%) - LeetHub

---
+++ 0412-fizz-buzz/0412-fizz-buzz.go
@@ -0,0 +1,18 @@
+func fizzBuzz(n int) []string {
+    result := make([]string, 0, n)
+
+	for i := 1; i <= n; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			result = append(result, "FizzBuzz")
+		case i%3 == 0:
+			result = append(result, "Fizz")
+		case i%5 == 0:
+			result = append(result, "Buzz")
+		default:
+			result = append(result, strconv.Itoa(i))
+		}
+	}
+
+	return result
+}
